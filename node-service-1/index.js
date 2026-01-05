40759f93c5e35a51a665ba9897def7df4e9c70e8
Tangala123 <98816309+Tangala123@users.noreply.github.com>
2026-01-05 14:32:13 +0530

    Update index.js

--- node-service-1/index.js
+++ node-service-1/index.js
@@ -1,1 +1,11 @@
 
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ service: "node-service-1", status: "running" });
+});
+
+app.listen(3000, () => {
+  console.log("node-service-1 running on port 3000");
+});
