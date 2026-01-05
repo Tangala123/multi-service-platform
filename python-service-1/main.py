2de1aa3174686cbfa9dbb2674088fa69a4b02429
Tangala123 <98816309+Tangala123@users.noreply.github.com>
2026-01-05 14:37:18 +0530

    Update main.py

--- python-service-1/main.py
+++ python-service-1/main.py
@@ -1,1 +1,11 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify(service="python-service-1", status="running")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
+  
