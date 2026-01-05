27eff0f2dd6328c207741d924423b468c540f54b
Tangala123 <98816309+Tangala123@users.noreply.github.com>
2026-01-05 14:40:03 +0530

    Update index.js

--- react-service-1/src/index.js
+++ react-service-1/src/index.js
@@ -1,1 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<h1>Hello from React Service 1</h1>);
 
